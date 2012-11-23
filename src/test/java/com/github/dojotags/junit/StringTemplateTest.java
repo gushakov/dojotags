f034a61ae1d20ec9c2ec00c105f5fce19159630f
George Ushakov <gushakov@yahoo.com>
2012-11-23 18:07:21 +0100

    unit test

--- src/test/java/com/github/dojotags/junit/StringTemplateTest.java
+++ src/test/java/com/github/dojotags/junit/StringTemplateTest.java
@@ -12,7 +12,7 @@ public class StringTemplateTest {
 	
 	@Test
 	public void testSubstitute() throws Exception {
-		String template = IOUtils.toString(getClass().getClassLoader().getResourceAsStream("com/github/dojotags/tags/templates/config.html"));
+		String template = IOUtils.toString(getClass().getClassLoader().getResourceAsStream("com/github/dojotags/tags/templates/page-begin.html"));
 		assertNotNull(template);
 		ST st = new ST(template, '$', '$');
 		st.add("theme", Constants.THEME_DEFAULT);
